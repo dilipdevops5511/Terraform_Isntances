d6683927e29a84679444217c0958afb7c54aaccd
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-19 11:28:30 +0530

    first commit

--- main.tf
+++ main.tf
@@ -13,3 +13,12 @@ resource "aws_subnet" "public_subnet" {
     Name = "Public Subnet"
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+tags = {
+    Name = "Private Subnet"
+  }
+}
